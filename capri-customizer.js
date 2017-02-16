c1272e420312cea466f104577c52ab43f3b561b3
Adam Norrell <dnorrell@industrialdefender.com>
2017-02-15 20:57:11 -0700

    Updating page with stripe selection

--- capri-customizer.js
+++ capri-customizer.js
@@ -15,4 +15,13 @@ $(function() {
 			$("#camperTrim").css("background-image", "url(img/retreat-8/r8-black-trim.png)");
 		}
 	});
+
+	// Change camper stripes color on swatch click
+	$("img.color-swatch-stripes").on("click", function() {
+		var color_name = $(this).attr("data-color");
+		var stripes_style = $(this).attr("data-stripes");
+		var stripes_img = "url(img/retreat-8/" + stripes_style + "/r8-" + stripes_style + "-" + color_name + ".png)";
+
+		$("#camperStripes").css("background-image", stripes_img);
+	});	
 });
